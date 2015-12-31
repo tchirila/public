bab3cb471e4e5c33f77d2f7141af0a6e6a58cfc4
Tiberiu Chirila <tibi_chirila@yahoo.com>
2015-12-31 12:04:26 +0000

    Update atmlist.py

--- atmlist.py
+++ atmlist.py
@@ -1,12 +1,33 @@
-#make a text list of files
+# make a text list of files, not good for indendation using iphyton, does not work in python2.7
 import os
+import os.path
 
 atmlist = []
-path = "//scratch//asttchir//apogee//apogeework//apogee//spectro//redux//speclib//kurucz_filled"
-for file in os.listdir(path):
-    if file.endswith(".mod"):
-        atmlist.append(file)
+path = "/scratch/asttchir/apogee/apogeework/apogee/spectro/redux/speclib/kurucz_filled"
+for path, dirnames, filenames in os.walk(path):
+    for file in [f for f in filenames if f.endswith(".mod")]:
+        atmlist.append(file)        
 outfile = open('atmlist.txt','w')
-for name in atmlist:
-    outfile.write(name+'\n')
+for name in atmlist:    
+    outfile.write(name+'\n')    
+outfile.close()
+
+
+# it's the same code but formatted for correct indentation in ipython when copy+pasting it there
+import os
+import os.path
+
+atmlist = []
+path = "/scratch/asttchir/apogee/apogeework/apogee/spectro/redux/speclib/kurucz_filled"
+for path, dirnames, filenames in os.walk(path):
+    
+    for file in [f for f in filenames if f.endswith(".mod")]:
+    
+        atmlist.append(file)        
+
+outfile = open('atmlist.txt','w')
+for name in atmlist:    
+
+    outfile.write(name+'\n')    
+
 outfile.close()
