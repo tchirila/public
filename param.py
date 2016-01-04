200fe6fda2aadc0e92534a556fe39a5999b14088
Tiberiu Chirila <tibi_chirila@yahoo.com>
2016-01-04 14:41:48 +0000

    Update param.py

--- param.py
+++ param.py
@@ -4,18 +4,23 @@ path2 = "D:\\Program Files\\Dropbox\\Python\\m4500m075_Si+025.par"
 with open(path) as atmfile:
     atmdata = atmfile.read()
     atmlist = atmdata.split() # each .mod name has 28 characters including the '.mod' part
-    i = 0
-    modelatm = atmlist[i]
-    param = modelatm[0:24]
-    t = modelatm[14:18]
-    g = 'g'+modelatm[19:21]
-    m = 'm'+modelatm[2:5]
+    n = int(1+len(atmlist)) # calculates nr. of elements and converts string to integer
+    for i in range(0, n, 1):
+        modelatm = atmlist[i]
+        param = modelatm[0:24]
+        t = modelatm[14:18]
+        g = 'g'+modelatm[19:21]
+        m = 'm'+modelatm[2:5]
 
-    with open(path2, 'r') as template, open(param+'_Al.par', 'w') as paramfile:
-        for line in template:
-            if line.strip() == 'standard_out   out1_s4500g20m075_sip025':
-                paramfile.write('standard_out   out1_s'+t+g+m+'\n')
-                if line.strip() == "model_in      'amm08cp00op00t4500g20v20.moog'":
+        with open(path2, 'r') as template, open(param+'_Al.par', 'w') as paramfile:
+            for line in template:
+                if line.strip() == 'standard_out   out1_s4500g20m075_sip025': 
+                    paramfile.write('standard_out   out1_s'+t+g+m+'_al'+'\n')
+                elif line.strip() == 'summary_out    out2_s4500g20m075_sip025': 
+                    paramfile.write('summary_out    out2_s'+t+g+m+'_al'+'\n') 
+                elif line.strip() == 'iraf_out       s4500g20m075_sip025.txt': 
+                    paramfile.write('iraf_out       s'+t+g+m+'_al.txt'+'\n')
+                elif line.strip() == "model_in      'amm08cp00op00t4500g20v20.moog'":
                     paramfile.write('model_in      '+"'"+modelatm+"'"+'\n')
-            else:
-                paramfile.write(line)
+                else:
+                    paramfile.write(line)
