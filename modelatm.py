026bb957ffab29f6078e7a1fdc678a75d2a3273c
Tiberiu Chirila <tibi_chirila@yahoo.com>
2016-02-02 10:53:42 +0000

    Update modelatm.py

--- modelatm.py
+++ modelatm.py
@@ -6,3 +6,15 @@ for t in range(3500, 6250, 250):
         for y in range(-2, 5, 1):
             m = y/10.0
             atm= atlas9.Atlas9Atmosphere(teff=t,logg=g,metals=m,am=0.25,cm=0.25)
+
+# new version, not tested yet
+from apogee.modelatm import atlas9
+try:
+    for t in range(3500, 6250, 250):
+        for x in range(0, 55, 5):
+            g = x/10.0 
+            for y in range(-2, 5, 1):
+                m = y/10.0
+                atm= atlas9.Atlas9Atmosphere(teff=t,logg=g,metals=m,am=0.25,cm=0.25)
+except KeyError:
+    pass
