fefe3971e76cf79c206782a1ae6d0effd2baae38
Tiberiu Chirila <tibi_chirila@yahoo.com>
2016-02-08 20:03:45 +0000

    Update modelatm.py

--- modelatm.py
+++ modelatm.py
@@ -1,21 +1,32 @@
 # downloads all files within parameter range (all combinations), use python2.7
 from apogee.modelatm import atlas9
-for t in range(3500, 6250, 250):
+import sys
+
+for t in range(3500, 6750, 250):
     for x in range(0, 55, 5):
         g = x/10.0 
-        for y in range(-2, 5, 1):
+        for y in range(-25, 10, 5):
             m = y/10.0
-            atm= atlas9.Atlas9Atmosphere(teff=t,logg=g,metals=m,am=0.25,cm=0.25)
+            try:
+                atm= atlas9.Atlas9Atmosphere(teff=t,logg=g,metals=m,am=0.25,cm=0.25)
+            except KeyError:
+                sys.exc_clear()
 
 # new version, not tested yet
+
 from apogee.modelatm import atlas9
 import sys
-try:
-    for t in range(3500, 6250, 250):
-        for x in range(0, 55, 5):
-            g = x/10.0 
-            for y in range(-2, 5, 1):
-                m = y/10.0
+
+for t in range(3500, 6750, 250):
+    
+    for x in range(0, 55, 5):
+        g = x/10.0 
+        
+        for y in range(-25, 10, 5):
+            m = y/10.0
+            
+            try:
                 atm= atlas9.Atlas9Atmosphere(teff=t,logg=g,metals=m,am=0.25,cm=0.25)
-except KeyError:
-    sys.exc_clear()
+            
+            except KeyError:
+                sys.exc_clear()
