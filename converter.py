0b81d00ac6469660606d73012f6538987e134642
Tiberiu Chirila <tibi_chirila@yahoo.com>
2016-02-12 14:15:59 +0000

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,34 @@
+# converts kurucz model atmospheres using the awk script
+import os
+import os.path
+
+atmlist = []
+path = "/scratch/asttchir/modelatm_kurucz"
+
+for i in range(0, 0):
+    for path, dirnames, filenames in os.walk(path):
+        for file in [f for f in filenames if f.endswith(".mod")]:
+            file = os.path.join(path, file)
+            atmlist.append(file)        
+    outfile = open('/scratch/asttchir/specwork/atmlist_kurucz.txt','w')
+
+    for name in atmlist:    
+        outfile.write(name+'\n')    
+    outfile.close()
+
+listpath = "/scratch/asttchir/specwork/atmlist_kurucz.txt"
+
+with open(listpath) as atmfile:
+    atmdata = atmfile.read()
+    atmlist = atmdata.split() # each .mod name has 28 characters including the '.mod' part
+    n = int(len(atmlist)) # calculates nr. of elements and converts string to integer
+    
+    for i in range(0, n, 1): # iterates for each element(.mod file) of the list    
+        oldpath = atmlist[i]
+        fullatmname = oldpath[-28:]
+        name = fullatmname[0:24]
+        newpath = '/scratch/asttchir/modelatm/'
+    
+    input('awk -f mod2moog '+str(oldpath)+' > '+newpath+str(name)+'.moog')
+
+# /home/asttchir/Dropbox/Python/converter.py
