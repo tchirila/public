d35dd9cc7619e84b8b42f5851b5cda465cdfebe7
Tiberiu Chirila <tibi_chirila@yahoo.com>
2016-02-12 13:20:06 +0000

    Update atmlist.py

--- atmlist.py
+++ atmlist.py
@@ -1,9 +1,9 @@
-# make a text list of files, not good for indendation using iphyton, does not work in python2.7
+# make a text list of files, not good for indendation using iphyton
 import os
 import os.path
 
 atmlist = []
-path = "/scratch/asttchir/apogee/apogeework/apogee/spectro/redux/speclib/kurucz_filled"
+path = "/scratch/asttchir/modelatm"
 for path, dirnames, filenames in os.walk(path):
     for file in [f for f in filenames if f.endswith(".mod")]:
         file = os.path.join(path, file)
@@ -13,23 +13,4 @@ for name in atmlist:
     outfile.write(name+'\n')    
 outfile.close()
 
-
-# it's the same code but formatted for correct indentation in ipython when copy+pasting it there
-import os
-import os.path
-
-atmlist = []
-path = "/scratch/asttchir/apogee/apogeework/apogee/spectro/redux/speclib/kurucz_filled"
-for path, dirnames, filenames in os.walk(path):
-    
-    for file in [f for f in filenames if f.endswith(".mod")]:
-        
-        file = os.path.join(path, file)
-        atmlist.append(file)        
-
-outfile = open('atmlist.txt','w')
-for name in atmlist:    
-
-    outfile.write(name+'\n')    
-
-outfile.close()
+# /home/asttchir/Dropbox/Python/atmlist.py
